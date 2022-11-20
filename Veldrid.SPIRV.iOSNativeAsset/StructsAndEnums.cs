2d1d2db821b46c1ace8002eee5fe67f4ccb28e07
yang zhou <xtuzhouyang@outlook.com>
2022-11-20 10:26:14 +0800

    添加项目文件。

---
+++ Veldrid.SPIRV.iOSNativeAsset/StructsAndEnums.cs
@@ -0,0 +1,4 @@
+namespace Veldrid.SPIRV.iOSNativeAsset
+{
+
+}
---
+++ Veldrid.SPIRV.iOSNativeAsset/libveldrid-spirv-combined.linkwith.cs
@@ -0,0 +1,4 @@
+﻿using System;
+using ObjCRuntime;
+
+[assembly: LinkWith("libveldrid-spirv-combined.a", LinkTarget.Arm64, SmartLink = true, ForceLoad = true)]
